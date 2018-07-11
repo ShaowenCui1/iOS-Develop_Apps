a2ac5fd82af65de7432a159873c85241e5223d6c
cuishaowen <cui.sha@husky.neu.edu>
2018-07-11 18:13:37 -0400

    upload I Am Rich

---
+++ I AM Rich/I AM Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I AM Rich
+//
+//  Created by 崔少文 on 6/28/18.
+//  Copyright © 2018 崔少文. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
